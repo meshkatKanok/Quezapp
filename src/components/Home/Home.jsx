140b2cdac8818b587e85863c25d656d4510a0868
meshkatKanok <meshkatkanok12@gmail.com>
2022-10-12 10:08:41 +0600

    responsivepart

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,40 +1,42 @@
 import React from 'react';
- import Companyimg from '../../../src/quiz.jpg'
+import Companyimg from '../../../src/quiz.jpg'
 import { useLoaderData } from 'react-router-dom';
 import Cart from '../Card/Cart';
 import './Home.css'
 
 const Home = () => {
-    const quizData=useLoaderData();
-    // console.log(quizData.data)
-    const {data}=quizData
-  
- 
-    return (
-        <div>
+  const quizData = useLoaderData();
+  // console.log(quizData.data)
+  const { data } = quizData
 
-            
 
-            {/*---------------------------- */}
-            <div className="card hrader-cart ">
-  <div className="card-body d-flex align-items-center">
-  <img src={Companyimg} className="card-img-top img" alt="" />
-   <div>
-   <h5 className=" ">All Popular Web Development Quize</h5>
-    <p className=''>This quiz covers all of the topics learnt in the "Introduction to Web Development" e-learning environment. There are no trick questions and all of the answers have been given at some point in the online sessions </p>
-   </div>
-   
-  </div>
-</div>
-            {/*------------------------------ */}
-         <div className='Allquize-data'>
-{
-data.map(data=><Cart key={data.id} data={data}></Cart>)
-}
+  return (
+    <div>
+
+
+         
+      {/*---------------------------- */}
+      <div className="card hrader-cart ">
+        <div className="card-body">
+         <div className="row d-flex justify-content-center align-items-center">
+          <div className="col-md-6 col-sm-6"><img src={Companyimg} className="card-img-top img" alt="" /></div>
+          <div className="col-md-6 col-sm-6">
+          <h5 className='text-center text-lg-start'>All Popular Web Development Quize</h5>
+            <p className='text-center text-lg-start'>This quiz covers all of the topics learnt in the "Introduction to Web Development" e-learning environment. There are no trick questions and all of the answers have been given at some point in the online sessions </p>
+          </div>
+         </div>
+
         </div>
-          
-        </div>
-    );
+      </div>
+      {/*------------------------------ */}
+      <div className='Allquize-data'>
+        {
+          data.map(data => <Cart key={data.id} data={data}></Cart>)
+        }
+      </div>
+
+    </div>
+  );
 };
 
 export default Home;
--- src/components/quiezData/QuezData.js
+++ src/components/quiezData/QuezData.js
@@ -3,28 +3,37 @@ import Quizeotion from '../Quizoption/Quizeotion';
 import './QuezData.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
- const QuezData = ({quez}) => {
-    const {question,options,correctAnswer}=quez
-    const handleClick=(option)=>{
-        if(option===`${correctAnswer}`){
-            toast(`Correct Answer`,{
-                position:"top-center"
+import { BeakerIcon, EyeIcon } from '@heroicons/react/24/solid'
+const QuezData = ({ quez }) => {
+    const { question, options, correctAnswer } = quez
+    const handleClick = (option) => {
+        if (option === `${correctAnswer}`) {
+            toast(`Correct Answer`, {
+                position: "top-center"
             });
 
         }
-        else{
-            toast(`worng Answer`,{
-                position:'top-center'
+        else {
+            toast(`worng Answer`, {
+                position: 'top-center'
             })
         }
+    }
+    const correctAns = (correctAnswer) => {
+        toast(`Correct Ans: ${correctAnswer}`, {
+            position: "top-center"
+        })
     }
     return (
         <div>
             <div className="card quize-data m-auto mt-5 bg-black text-warning">
-                <p>{question}</p>
+                <div className='d-flex align-items-center justify-content-between'>
+                    <p>{question}</p>
+                    <EyeIcon onClick={() => correctAns(correctAnswer)} className="text-blue-500 eye-icone" />
+                </div>
                 <div className="card-body text-primary">
                     {
-                        options.map((option,index)=><Quizeotion quez={quez} key={index} handleClick={handleClick} option={option}></Quizeotion>)
+                        options.map((option, index) => <Quizeotion quez={quez} key={index} handleClick={handleClick} option={option}></Quizeotion>)
                     }
                 </div>
             </div>
