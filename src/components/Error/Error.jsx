bfe56f6d5eecaf886063d49c4e876611b53c26df
meshkatKanok <meshkatkanok12@gmail.com>
2022-10-12 22:46:20 +0600

    404found

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
- 
-
+import React from 'react'
 const Error = () => {
     return (
         <div>
-         this is found
+            <div className="card bg-gray w-50 m-auto mt-5 p-5cle">
+                    <div className="card-body text-center text-white">
+                        <h1>404</h1>
+                        <p>Page NoT Found</p>
+                        <p>We are sorry the page you requested could not be found please go back to the homepage</p>
+                       
+                    </div>
+            </div>
         </div>
     );
 };
